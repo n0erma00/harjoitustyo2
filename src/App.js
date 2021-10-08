60a516338e5c8ad580ebd11051a6707e2c17801f
Markus Erkkilä <n0erma00@students.oamk.fi>
2021-10-08 13:52:11 +0300

    ei onnistu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+const URL = 'https://animechan.vercel.app/api/random';
 
 function App() {
+
+  const [anime, setAnime] = useState('');
+  const [char, setChar] = useState('');
+  const [quo, setQuo] = useState('');
+
+  useEffect(() => {
+    fetch(URL)
+      .then(response => response.json())
+      .then(quote => console.log(quote))
+      
+  }, [])
+  
+
   return (
-    null
+    <div id="container">
+      <h1>Animequotet</h1>
+      <h2>Anime: {anime}</h2>
+      <h3>Hahmo: {char}</h3>
+      <p>Quote: {quo}</p>
+      <button>Uusi</button>
+    </div>
   );
 }
 
