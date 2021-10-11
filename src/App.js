1437d884def96af8512bbb6056fe2c70f9b6f3fd
Markus Erkkilä <n0erma00@students.oamk.fi>
2021-10-11 09:58:05 +0300

    toimiva versio

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import './App.css';
-import {useState, useEffect} from 'react';
+import './App.css'
+import { useState, useEffect } from 'react'
 
-const URL = 'https://animechan.vercel.app/api/random';
+const URL = 'https://animechan.vercel.app/api/random'
 
-function App() {
-
-  const [anime, setAnime] = useState('');
-  const [char, setChar] = useState('');
-  const [quo, setQuo] = useState('');
+function App () {
+  const [anime, setAnime] = useState('')
+  const [char, setChar] = useState('')
+  const [quo, setQuo] = useState('')
 
   useEffect(() => {
     fetch(URL)
       .then(response => response.json())
-      .then(quote => console.log(quote))
-      
+      .then(response => {
+        setAnime(response.anime)
+        setChar(response.character)
+        setQuo(response.quote)
+      })
   }, [])
-  
 
   return (
-    <div id="container">
-      <h1>Animequotet</h1>
-      <h2>Anime: {anime}</h2>
-      <h3>Hahmo: {char}</h3>
-      <p>Quote: {quo}</p>
-      <button>Uusi</button>
+    <div id='container'>
+      <h1>Anime quote of the day</h1>
+      <h2>Anime title: {anime}</h2>
+      <h3>Character: {char}</h3>
+      <p id="quote">Quote: " {quo} "</p>
+      <button onClick={() => window.location.reload(false)}>New Quote</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
